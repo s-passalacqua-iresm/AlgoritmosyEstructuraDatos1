9dea464c18df395bc4ede57c8e325897c527c33b
LucasMLioy <69652406+LucasMLioy@users.noreply.github.com>
2020-08-25 20:05:11 -0300

    Mover archivo a carpeta

--- ACTIVITY 1- EX 1.py
+++
@@ -1,9 +0,0 @@
-#Algotimo que lea entero y verifique sea mayor a 10. 
-#Si lo es, escribir el número, si no, mensaje que diga que es menor o igual
-
-numeroentero=int(input("Ingrese un número entero: "))
-
-if int(numeroentero) >10:
-    print(f"{numeroentero}")
-else:
-    print(f"El número ingresado es menor o igual a 10")
--- ACTIVITY 1- EX 3.py
+++
@@ -1,8 +0,0 @@
-#Convertir temperaturas de Fahrenheit a Celcius
-#Fórmula: C°= (5/9). (°F -32)
-
-tempingresada=float(input("Ingrese la temperatura en grados Farenheit: "))
-
-calculoconversion=5/9*(float(tempingresada)-32)
-
-print(f"La temperatura que usted ingresó en grados C° es igual a {calculoconversion}")
