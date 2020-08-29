3d8dabab3ddadf8dcf898893cd9875a52e1f3c56
LucasMLioy <69652406+LucasMLioy@users.noreply.github.com>
2020-08-29 02:44:22 -0300

    changes

--- Actividad 2- Funciones y menú/Funciones.py
+++ Actividad 2- Funciones y menú/Funciones.py
@@ -1,38 +1,33 @@
 #1: método sin retorno ni parámetros: chequear si num entero > 10
-
-def num_entero_mayor_diez(numenteroingresado):
-    if int(numeroentero) > 10:
-        print(f"{numeroentero}")
+def num_entero_mayor_diez(numIngresado):
+    if (numIngresado > 10):
+        print(f"{numIngresado}")
     else:
         print(f"El número ingresado es menor o igual a 10")
 
 #2: método sin retorno y con parámetros: recibir 3 números....
-
 def suma_tres_numeros(primernum=0, segundonum=0, tercernum=0):
 
-    if int(primernum) + int(segundonum) == int(tercernum):
-        print("La suma de los dos primeros números enteros es igual al tercero ingresado")
-    else:
-        print("La suma de los dos primeros números enteros no es igual al tercero ingresado")
+     suma = primernum + segundonum
+
+     if(suma == tercernum):
+         print("La suma de los dos primeros números enteros es igual al tercero ingresado")
+     else:
+         print("La suma de los dos primeros números enteros no es igual al tercero ingresado")
 
 #3: método con retorno y parámetros con 2 números enteros
+def division_de_cuadrados(numEntero1 , numEntero2 ):
 
-def division_de_cuadrados(numentero1=1, numentero2=1):
-
-    if int(numentero1)>int(numentero2):
-        calculo1=(int(numentero1)**2)/(int(numentero2)**2)
-        return(str((f"El resultado de la operación es: {calculo1}")))
-
-    elif int(numentero2)>int(numentero1):
-        calculo2=(int(numentero2)**2)/(int(numentero1)**2)
-        return(str(print(f"El resultado de la operación es: {calculo2}")))
-
+    if (numEntero1 > numEntero2):
+        calculo1 = (numEntero1*2) / (numEntero2*2)
+        return str(f"El resultado de la operación es: {calculo1}")
+    elif(numEntero2 >  numEntero1):
+        calculo2 = (numEntero2*2 ) / (numEntero1*2)
+        return str(f"El resultado de la operación es: {calculo2}")
     else:
-        return(str(print(f"Los números ingresados son iguales")))
-
+        return str(f"Los números ingresados son iguales;")
 
 #4: método con retorno y sin parámetros: convertir temperaturas de F a C
-
 def que_temperatura_es_en_celcius():
     farenheit=int(input("Ingrese la temperatura en Farenheit: "))
     celcius = int(5/9 * (farenheit - 32))
--- Actividad 2- Funciones y menú/Principal1.py
+++ Actividad 2- Funciones y menú/Principal1.py
@@ -1,37 +1,34 @@
 import funciones
 
 def menu ():
+
     print(" --------------------------------------MENU--------------------------------------")
     print("1. Número entero mayor a diez")
     print("2. Suma de tres números ")
     print("3. División de cuadrados")
     print("4. Convertir tempratura de Farenheit a Celcius")
     print("----------------------------------------------------------------------------------")
+    opIngresada = int(input("Ingrese la opción que desea realizar: "))
 
-opcioningresada=int(input("Ingrese el número de actividad que quiera realizar: "))
+    if(opIngresada == 1):
+        numIngresado = int(input("Ingrese un número entero: "))
+        funciones.num_entero_mayor_diez(numIngresado)
 
-if opcioningresada == 1:
-    numenteroingresado = int(input("Ingrese un número entero: "))
-    act1=funciones.num_entero_mayor_diez()
-    #return print(str(act1))
+    elif(opIngresada == 2):
+        primernum = int(input("Ingrese el primer número entero: "))
+        segundonum = int(input("Ingrese el segundo número entero: "))
+        tercernum = int(input("Ingrese el tercer número entero: "))
+        funciones.suma_tres_numeros(primernum, segundonum, tercernum)
 
-elif opcioningresada == 2:
-    primernum = int(input("Ingrese el primer número entero: "))
-    segundonum = int(input("Ingrese el segundo número entero: "))
-    tercernum = int(input("Ingrese el tercer número entero: "))
+    elif(opIngresada == 3):
+        num1 = int(input("Ingrese el primer número entero: "))
+        num2 = int(input("Ingrese el segundo número entero: "))
+        act3 = funciones.division_de_cuadrados(num1, num2)
+        return print(act3)
 
-    act2=funciones.suma_tres_numeros(num1=primernum, num2=segundonum, num3=tercernum)
-    #return print(str(act2))
-
-elif opcioningresada == 3:
-    numentero1 = int(input("Ingrese el primer número entero: "))
-    numentero2 = int(input("Ingrese el segundo número entero: "))
-
-    act3=funciones.division_de_cuadrados(num1=numentero1, num2=numentero2)
-    #return print(str(act3))
-
-elif opcioningresada == 4:
-    act4= funciones.que_temperatura_es_en_celcius()
-    #return print(str(f"{act4}°"))
+    elif (opIngresada == 4):
+        act4 = funciones.que_temperatura_es_en_celcius()
+        return print(act4)
 
 menu()
+
