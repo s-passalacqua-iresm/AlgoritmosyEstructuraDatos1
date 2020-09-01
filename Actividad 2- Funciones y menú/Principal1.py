4d2d5a4e1651c69bce6ddb82b9cf410f2c1210ba
s-passalacqua-iresm <69656420+s-passalacqua-iresm@users.noreply.github.com>
2020-08-31 22:51:11 -0300

    Update Principal1.py

--- Actividad 2- Funciones y menú/Principal1.py
+++ Actividad 2- Funciones y menú/Principal1.py
@@ -12,23 +12,23 @@ def menu ():
 
     if(opIngresada == 1):
         numIngresado = int(input("Ingrese un número entero: "))
-        funciones.num_entero_mayor_diez(numIngresado)
+        funciones.num_entero_mayor_diez(numIngresado) # La funcion no debe recibir parametros el numero debe pedirse directamente en la funcion
 
     elif(opIngresada == 2):
         primernum = int(input("Ingrese el primer número entero: "))
         segundonum = int(input("Ingrese el segundo número entero: "))
         tercernum = int(input("Ingrese el tercer número entero: "))
-        funciones.suma_tres_numeros(primernum, segundonum, tercernum)
+        funciones.suma_tres_numeros(primernum, segundonum, tercernum) # OK
 
     elif(opIngresada == 3):
         num1 = int(input("Ingrese el primer número entero: "))
         num2 = int(input("Ingrese el segundo número entero: "))
         act3 = funciones.division_de_cuadrados(num1, num2)
-        return print(act3)
+        return print(act3) # Esta linea deberia ser: print(f"El resultado de la operación es: {act3}"), sin usar la palabra reservada return
 
     elif (opIngresada == 4):
         act4 = funciones.que_temperatura_es_en_celcius()
-        return print(act4)
+        return print(act4) # Esta linea deberia ser: print(f"La temperatura en celcius es de: {act4}°"), sin usar la palabra reservada return
 
 menu()
 
